27de745e6041a43c4fa6c13737d73073bac24cc8
sammy <sammylaureys@hotmail.com>
2021-06-17 13:14:31 +0200

    widget gemaakt voor de cards in gordon dart

--- lib/gordon.dart
+++ lib/gordon.dart
@@ -70,32 +70,7 @@ class AppTreeState extends State<AppTree> {
           child: VideoPlayer(videoController),
         ),
       ),
-      Column(
-        children: [
-          for (var i = 0; i < lib.quotes.length; i++)
-            Card(
-               margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0),
-               color: Colors.white,
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-            child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(
-                      "" + lib.quotes[i]['picture']), // no matter how big it is, it won't overflow
-                    ),
-                    title: Text("" + lib.quotes[i]['quote'],
-                      style: TextStyle(color: Colors.grey)),
-          ),
-        ],
-      ),
-    ),
-        ],
-      ),
+      createCards,
       RaisedButton(
           child: Icon(Icons.arrow_right),
           onPressed: () { audioPlayer.play('sounds/dogFood.mp3', volume: 100); },
@@ -107,7 +82,33 @@ class AppTreeState extends State<AppTree> {
   }
 }
 
-
+Widget createCards =
+Column(
+  children: [
+    for (var i = 0; i < lib.quotes.length; i++)
+      Card(
+        margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0),
+        color: Colors.white,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    "" + lib.quotes[i]['picture']), // no matter how big it is, it won't overflow
+              ),
+              title: Text("" + lib.quotes[i]['quote'],
+                  style: TextStyle(color: Colors.grey)),
+            ),
+          ],
+        ),
+      ),
+  ],
+);
 
 
 
